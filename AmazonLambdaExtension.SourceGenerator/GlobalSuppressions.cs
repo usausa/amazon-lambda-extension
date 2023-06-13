0c9979dcc5d15e311e1b31e13924ba06baf1d3b0
usausa <machi.pon@gmail.com>
2023-06-13 15:17:05 +0900

    Update setting

---
+++ AmazonLambdaExtension.SourceGenerator/GlobalSuppressions.cs
@@ -0,0 +1,2 @@
+// TODO https://github.com/dotnet/roslyn-analyzers/issues/6467
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("MicrosoftCodeAnalysisCorrectness", "RS1035: Do not use APIs banned for analyzers", Justification = "False positive")]
